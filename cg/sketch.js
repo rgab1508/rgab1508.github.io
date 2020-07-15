5b7f04f85dd8d61907e3b39a453cf34df9766b25
Gabriel <rgabriel1508@gmail.com>
2020-07-15 20:36:34 +0530

    update skect.js

--- cg/sketch.js
+++ cg/sketch.js
@@ -3,7 +3,13 @@ let start=[];
 let end = [];
 var state= '';
 
-
+let col = {
+	'START':color(100, 100, 0),
+	'END': color(0, 100, 50),
+	'YMX': color(255, 0, 0),
+	'DDA': color(0, 255, 0),
+	'BRE': color(0, 0, 255),
+}
 
 function ymx(x1, y1, x2, y2){
   let points = [];
@@ -12,7 +18,7 @@ function ymx(x1, y1, x2, y2){
   
   for(let x=x1;x<=x2;x++){
     let y = m*x + b;
-    points.push([Math.ceil(x), Math.ceil(y)]);
+    points.push([Math.floor(x), Math.floor(y)]);
   }
   return points;
 }
@@ -24,9 +30,10 @@ function calculateLine(){
   let points = ymx(start[0],start[1], end[0], end[1]);
   for(let i=0;i<points.length;i++){
     let el = points[i];
-    grid.pixels[el[0]][el[1]].col = 0;
+    grid.pixels[el[0]][el[1]].col = col['YMX'];
   }
 }
+
 
 
 
@@ -37,7 +44,11 @@ function setup(){
   let b = createButton('Calculate' );
   b.mousePressed(calculateLine);
   state = 'START';
-  console.log(state)
+  console.log(state);
+  let sButton = createButton('start point');
+  let eButton = createButton('end Point');
+  sButton.mousePressed(function(){state='START'});
+  eButton.nousePressed(function(){state='END'});
 }
 
 
@@ -49,8 +60,10 @@ function mousePressed(){
     if(state == 'START'){
       start = pp;
       state = 'END';
+      grid.pixels[pp[0]][pp[1]].col = col['START'];
     }else if(state == 'END'){
       end = pp;
+      grid.pixels[pp[0]][pp[1]].col = col['END'];
     }
   }
   console.log('start', start);
