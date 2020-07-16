fef8a01d649054b9bd51735174616471947ec9d0
rgab1508 <rgabriel1508@gmail.com>
2020-07-16 14:27:17 +0530

    Updated ymx.js

--- cg/ymx.js
+++ cg/ymx.js
@@ -1,13 +1,13 @@
 function ymx(x1, y1, x2, y2) {
   let points = [];
-  if(x2 > x1){
+  /*if(x2 > x1){
     let px = x2;
     x2 = x1;
     x1 = px;
     let py = y2;
     y2 = y1;
     y1 = py;
-  }
+  }*/
   let m = (y2 - y1) / (x2 - x1);
   let b = y1 - m * x1;
 
