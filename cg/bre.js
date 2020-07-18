466389e854e2443fe84265214bf751a71d2409cb
rgab1508 <rgabriel1508@gmail.com>
2020-07-18 21:32:53 +0530

    Updated bre.js

--- cg/bre.js
+++ cg/bre.js
@@ -1,19 +1,45 @@
-function bre(x1, y1, x2, y2){
+function preBre(x1, y1, x2, y2) {
   let points = [];
   let dx = x2 - x1;
   let dy = y2 - y1;
-  
-  let error = 2*dy - dx;
+
+  let error = 2 * dy - dx;
   let y = y1;
-  for(let x=x1;x<=x2;x++){
+  for (let x = x1; x <= x2; x++) {
     points.push([x, y]);
-    if(error > 0){
-      y = y+1;
-      error = error - 2 *dx;
+    if (error > 0) {
+      y = y + 1;
+      error = error - 2 * dx;
     }
-    error = error + 2 *dy;
+    error = error + 2 * dy;
   }
   return points;
 }
 
 //console.log(bre(1, 1, 5, 5));
+
+function bre(x1, y1, x2, y2) {
+  let points = [];
+  let dx = Math.abs(x2 - x1);
+  let sx = x1 < x2 ? 1 : -1;
+  let dy = -Math.abs(y2 - y1);
+  let sy = y1 < y2 ? 1 : -1;
+  let err = dx + dy;
+
+  while (true) {
+    points.push([x1, y1]);
+    if (x1 == x2 && y1 == y2) {
+      break;
+    }
+    let e2 = 2 * err;
+    if (e2 >= dy) {
+      err += dy;
+      x1 += sx;
+    }
+    if (e2 <= dx) {
+      err += dx;
+      y1 += sy;
+    }
+  }
+  return points;
+}
