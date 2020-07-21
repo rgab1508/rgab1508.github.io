a36c6838d8fe6623459372b1ed1bf85537037c85
rgab1508 <rgabriel1508@gmail.com>
2020-07-21 19:38:44 +0530

    added k1

---
+++ cg/Kaleido/k1/sketch.js
@@ -0,0 +1,12 @@
+function setup(){
+  createCanvas(400,400);
+  background(51);
+}
+
+function draw(){
+  //background(51);
+  stroke(255)
+  line(50,50, 200,350);
+  line(200,350,350,50);
+  line(350, 50, 50, 50);
+}
