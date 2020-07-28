d4cd2b15458417e3113f23faf94bef4987061abf
rgab1508 <rgabriel1508@gmail.com>
2020-07-28 21:28:22 +0530

    Updated cg/matrix index.html, sketch.js

--- cg/matrix/sketch.js
+++ cg/matrix/sketch.js
@@ -1,7 +1,7 @@
 let x = 10 , y = 0;
-let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789%$#@!^&~';
 let charGrid = new Array();
-let charW = 20, charH = 20;
+let charW = 25, charH = 25;
 let nWChar, nHChar;
 let tops = [];
 
@@ -11,14 +11,18 @@ function pickChar(){
 
 class Topper{
   constructor(x) { 
+    this.x = x;
     this.arr = [];
-    let y = 0;
-    for(let i=0;i<nWChar;i++){
+    this.speed = (Math.random() * 2)+1;
+    let y = -(Math.floor(Math.random()*150)-1);
+    let n = Math.floor((nWChar + nHChar)/2)
+    for(let i=0;i<n;i++){
       let c = pickChar();
       let cObj = {
-        x: x,
+        x: this.x,
         y: y,
-        char: c
+        char: c,
+        size : Math.random() * (charW - 10) + 10
       }
       y -= charH;
       this.arr.push(cObj);
@@ -28,16 +32,20 @@ class Topper{
   
   update(){
     for(let a of this.arr){
-      a.y += 1;
+      a.y += this.speed;
     }
   }
   
   show(){
-    textSize(charH);
     for(let a of this.arr){
+      textSize(a.size);
       stroke(0, 255, 0);
       text(a.char, a.x, a.y);
     }
+  }
+  
+  ends(){
+    return this.arr[this.arr.length - 1].y > height;
   }
 }
 
@@ -58,6 +66,12 @@ function draw(){
   for(let t of tops){
     t.update();
     t.show();
+  }
+  for(let i=0;i<tops.length;i++){
+    if(tops[i].ends()){
+      let x = tops[i].x;
+      tops[i]= new Topper(x);
+    }
   }
   // for(let i = 0;i<width;i+=1){
   //   text("G", x*i, y-10);
