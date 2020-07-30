744e76300c630be5976be6bb0121479a24fec3da
rgab1508 <rgabriel1508@gmail.com>
2020-07-30 13:19:44 +0530

    Updated cg/ellipse/sketch.js

--- cg/ellipse/mpea.js
+++ cg/ellipse/mpea.js
@@ -14,7 +14,7 @@ function region1(x0, y0, rx, ry){
     }else{
       p = 0;
       x++;
-      y++;
+      y--;
       p = p + 2*pow2(ry)*x - 2*pow2(rx)*y + pow2(ry);
     }
     points.push([x, y]);
@@ -23,9 +23,22 @@ function region1(x0, y0, rx, ry){
 }
 
 function region2(x0, y0, rx, ry){
-  //@TODO
   let points = [];
+  let x = x0;
+  let y = y0;
   
+  p = pow2(ry)*pow2(x0+0.5)+pow2(rx)*pow2(y0-1)-pow2(rx)*pow2(ry);
+  while(y!= 0){
+    if(p >0){
+      y--;
+      p = p - 2*pow2(rx)*(y)+pow2(rx);
+    }else {
+      x++;
+      y--;
+      p = p + 2*pow2(ry)*x - 2*pow2(rx)*y + pow2(rx);
+    }
+    points.push([x, y]);
+  }
   return points;
   
 }
@@ -35,8 +48,39 @@ function mpea(x, y, rx, ry){
     Midpoint Ellipse Algorithm
   */
   let points = [];
+  points.push([0, ry]);
   let r1Points = region1(x, y, rx, ry);
-  Array.prototype.apply(points, r1Points);
+  Array.prototype.push.apply(points, r1Points);
+  let r2Points = region2(points[points.length-1][0], points[points.length-1][1], rx, ry);
+  Array.prototype.push.apply(points, r2Points);
+  console.log(points);
+  
+  points = sym4(points);
+  for(let p of points){
+    p[0] += x;
+    p[1] += y;
+  }
   return points;
 }
 
+function sym4(points){
+  let nPoints = [];
+  Array.prototype.push.apply(nPoints, points);
+  
+  for(let p of points){
+    nPoints.push([p[0], -p[1]]);
+  }
+  for (let p of points) {
+    nPoints.push([-p[0], -p[1]]);
+  }
+  for (let p of points) {
+    nPoints.push([-p[0], p[1]]);
+  }
+  return nPoints;
+}
+
+function test(){
+  console.log('ellipse: ', mpea(10, 10, 5, 5));
+}
+
+//test();
--- cg/ellipse/sketch.js
+++ cg/ellipse/sketch.js
@@ -1,19 +1,25 @@
 let grid;
-let n = 10;
+let n = 20;
 
 let x, y, rx, ry;
 let xInp, yInp, rxInp, ryInp;
 
 function calcEllipse(x, y, rx, ry){
+  grid.clear();
   let points = mpea(x, y, rx, ry);
   for(let p of points){
+    console.log(p);
     grid.pixels[p[0]][p[1]].col = color(51);
   }
 }
 
 
 function setup(){
-  let c = createCanvas(400, 400);
+  x = 10;
+  y = 10;
+  rx = 5;
+  ry = 5;
+  let c = createCanvas(600, 600);
   c.parent(select('#cnvs'));
   
   grid = new Grid(n, width, height);
@@ -26,14 +32,14 @@ function setup(){
   rxInp = select('#rx');
   rxInp.value(rx);
   ryInp = select('#ry');
-  ryINp.value(ry);
+  ryInp.value(ry);
   let calcButton = select("#calc");
   
   calcButton.mousePressed(function(){
-    x = xInp.value();
-    y = yInp.value();
-    rx = rxInp.value();
-    ry = ryInp.value();
+    x = parseInt(xInp.value());
+    y = parseInt(yInp.value());
+    rx = parseInt(rxInp.value());
+    ry = parseInt(ryInp.value());
   
     
     calcEllipse(x, y, rx, ry);
